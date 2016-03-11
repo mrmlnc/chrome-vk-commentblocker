ee093a17be023459efaed603f5eda892e92efb9f
Denis Malinochkin <dmalinochkin@rambler.ru>
2016-03-11 17:52:58 +0300

    Remove built files

--- extension/scripts/background.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (changeInfo && changeInfo.status === 'complete' && tab.url.indexOf('://vk.com/') > -1) {
-    chrome.tabs.executeScript({
-      file: 'scripts/inject.js'
-    });
-  }
-});
